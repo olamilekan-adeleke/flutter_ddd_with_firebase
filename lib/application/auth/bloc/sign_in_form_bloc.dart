a54c7f90d182e89ceb837d52fb1389d368ed0936
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-05-08 20:21:34 +0100

    feat: added bloc

---
+++ lib/application/auth/bloc/sign_in_form_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../domain/auth/facade/I_auth_facade.dart';
+import '../../../domain/auth/failure/auth_failure.dart';
+import '../../../domain/auth/value_object.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade iAuthFacade;
+
+  SignInFormBloc({
+    required this.iAuthFacade,
+  }) : super(SignInFormState.initial()) {
+    on<SignInFormEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
