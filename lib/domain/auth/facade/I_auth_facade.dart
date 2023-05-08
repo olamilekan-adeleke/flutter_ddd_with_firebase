440f7226e0cb588d4f25e5fda2f9cef5a3bd3061
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2023-05-08 17:23:03 +0100

    feat: added facade interface

---
+++ lib/domain/auth/facade/I_auth_facade.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import '../failure/auth_failure.dart';
+import '../value_object.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
